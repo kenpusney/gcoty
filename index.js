1ddefc58e7ea0e50027ab44d0af38fb74fb30be4
KimmyLeo <kenpusney@gmail.com>
2020-10-15 14:58:36 +0800

    add ics calendar generating

--- index.js
+++ index.js
@@ -9,7 +9,35 @@ const {
     renderHTMLPage
 } = require("./render");
 
-async function render(year = "2020", htmlFile = "index.html", dataFile = "data.json") {
+const moment = require("moment")
+
+const ical = require("ical-generator");
+
+const {createHash}= require('crypto');
+/**
+ * @param {string} algorithm
+ * @param {any} content
+ *  @return {string}
+ */
+const encrypt = (algorithm, content) => {
+  let hash = createHash(algorithm)
+  hash.update(content)
+  return hash.digest('hex')
+}
+/**
+ * @param {any} content
+ *  @return {string}
+ */
+const sha1 = (content) => encrypt('sha1', content)
+
+function gameEventDescription(platforms, url) {
+    return `
+Platforms: ${platforms.join(", ")}
+Url: ${url}
+`
+}
+
+async function render(year = "2020", htmlFile = "public/index.html", dataFile = "public/data.json", icsFile = "public/event.ics") {
     const response = await fetch(`https://www.gameinformer.com/${year}`);
 
     const body = await response.text()
@@ -20,6 +48,8 @@ async function render(year = "2020", htmlFile = "index.html", dataFile = "data.j
         return Array.prototype.slice.apply(list)
     };
     const calender_entries = root.querySelectorAll(".calendar_entry");
+
+    const cal = ical({domain: "kimleo.net", name: "Game Calendar of the Year"}).timezone("Asia/Shanghai");
 
     const result = calender_entries.map(it => {
         const link = it.querySelector("a");
@@ -33,11 +63,24 @@ async function render(year = "2020", htmlFile = "index.html", dataFile = "data.j
                 .map(tag => tag.text.split(", "))
                 .reduce((a, b) => a.concat(b), []);
 
+
+        if (releaseDate !== undefined) {
+            cal.createEvent({
+                start: moment(new Date(`${releaseDate} ${year}`)),
+                uid: sha1(`${title}-${releaseDate}-${platforms.join("-")}`),
+                allDay: true,
+                summary: title,
+                description: gameEventDescription(platforms, `https://www.gameinformer.com${url}`),
+                url: `https://www.gameinformer.com${url}`
+            });
+        }
+        
         return {
             title, url, releaseDate, platforms
         }
     })
 
+    fs.writeFileSync(icsFile, cal.toString());
 
     fs.writeFileSync(htmlFile, renderHTMLPage(renderGameList(result, year), year));
 
@@ -45,6 +88,6 @@ async function render(year = "2020", htmlFile = "index.html", dataFile = "data.j
 }
 
 (async() => {
-    await render("2020", "public/index.html", "public/data.json");
-    await render("2021", "public/2021.html", "public/2021.json");
+    await render("2020", "public/index.html", "public/data.json", "public/2020.ics");
+    await render("2021", "public/2021.html", "public/2021.json", "public/2021.ics");
 })();
